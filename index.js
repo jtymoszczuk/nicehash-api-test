b9e6f517a4ff820ffc274ababd2025f6232d905b
joe <joetymo@gmail.com>
2023-05-02 20:29:07 -0400

    edits

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-require = require("esm")(module/*, options*/);
+import Api from './api.js';
+import config from './config.js';
 
-// examples
-//require("./hashpower");
-//require("./exchange");
+const api = new Api(config);
+
+async function getBalance() {
+  await api.getTime(); // get server time - required
+  const { totalBalance } = await api.get(
+    "/main/api/v2/accounting/account2/BTC",
+  );
+  console.log(`NiceHash total balance: ${totalBalance} BTC`);
+}
+
+getBalance();
